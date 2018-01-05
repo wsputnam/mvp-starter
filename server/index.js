e6714a79933658253d19b9a80afc491529a4aa86
Your Name <williamputnamjr@gmail.com>
2018-01-04 17:53:16 -0800

    commiting while things still work

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ var bodyParser = require('body-parser');
 // var items = require('../database-mongo');
 
 var app = express();
+var request = require('request');
 
-var items = [{"qotd_date":"2018-01-05T00:00:00.000+00:00","quote":{"id":27654,"dialogue":false,"private":false,"tags":["good","wisdom"],"url":"https://favqs.com/quotes/francois-de-la-rochefoucauld/27654-few-people-ha-","favorites_count":1,"upvotes_count":1,"downvotes_count":0,"author":"Francois de La Rochefoucauld","author_permalink":"francois-de-la-rochefoucauld","body":"Few people have the wisdom to prefer the criticism that would do them good, to the praise that deceives them."}}, {
+var items = [{"qotd_date":"2018-01-05T00:00:00.000+00:00","quote":{"id":38713,"dialogue":false,"private":false,"tags":["love"],"url":"https://favqs.com/quotes/friedrich-nietzsche/38713-there-is-alwa-","favorites_count":1,"upvotes_count":1,"downvotes_count":0,"author":"Friedrich Nietzsche","author_permalink":"friedrich-nietzsche","body":"There is always some madness in love. But there is also always some reason in madness."}}, {"qotd_date":"2018-01-05T00:00:00.000+00:00","quote":{"id":27654,"dialogue":false,"private":false,"tags":["good","wisdom"],"url":"https://favqs.com/quotes/francois-de-la-rochefoucauld/27654-few-people-ha-","favorites_count":1,"upvotes_count":1,"downvotes_count":0,"author":"Francois de La Rochefoucauld","author_permalink":"francois-de-la-rochefoucauld","body":"Few people have the wisdom to prefer the criticism that would do them good, to the praise that deceives them."}}, {
   "quote": {
     "author": "Mark Twain",
     "body": "Never let your schooling interfere with your education."
@@ -18,6 +19,9 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
+// var options = {
+// 	url: `https://favqs.com/${API}/quotes/`
+// };
 app.post('/items', function(req, res) {
   if (err) {
   	res.sendStatus(404);
@@ -26,7 +30,7 @@ app.post('/items', function(req, res) {
   }
 });
 app.get('/items', function (req, res) {
- 
+
       res.json(items);
    
   
